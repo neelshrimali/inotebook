f5ea6f21b1ad48d91554a508236f3c1fd1a2a338
NeelShrimali <neelshrimali1994@gmail.com>
2023-02-14 16:58:58 +0530

    Update note api added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,17 +6,16 @@ const { body, validationResult } = require("express-validator");
 
 //Route 1: Get a all notes using: POST "/api/notes/fetchallnotes". login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
-    try {
-        const notes = await Notes.find({ user: req.user.id });
-  res.json(notes);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal server error..");
-    }
-  
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error..");
+  }
 });
 
-//Route 5: add new note using: POST "/api/notes/addnote". login required
+//Route 2: add new note using: POST "/api/notes/addnote". login required
 router.post(
   "/addnote",
   fetchuser,
@@ -41,12 +40,41 @@ router.post(
 
       const savedNote = await note.save();
       res.json(savedNote);
-      
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error..");
     }
   }
 );
+//Route 3: update an existing note using: POST "/api/notes/updatenote". login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  //create a newnote object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+  //find the note to be updated and update it
+  let note = await Notes.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed");
+  }
+
+  note = await Notes.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({ note });
+});
 
 module.exports = router;
