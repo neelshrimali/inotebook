496ee9e69ab4a91c40c11653c028c68620cd225e
NeelShrimali <neelshrimali1994@gmail.com>
2023-02-21 17:30:54 +0530

    Normal words added in About page

--- src/component/About.js
+++ src/component/About.js
@@ -2,9 +2,27 @@ const About = () => {
   
   
   return (
-    <div>
-      i am about comp.
+    <div className="container">    
+    <div className="text-white aboutImg text-center">
+        <div className="note-img">
+            <p>An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee</p>
+        </div>
     </div>
+    <div className="container mt-5 ">
+        <div className="row">
+            <div className="col-md-6 d-flex flex-column justify-content-center">
+                <h2 className="mb-3" style={{ fontWeight: "Bold" }}>Make something <span style={{ color: "#9C27B0" }}>Awesome</span> </h2>
+                <p>MyNotebook is made from the pain of writing all the things in notebook which is very hectic :(, So we mad an online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.
+                    you can also access your notes anywhere in your world, at anytime time . So dont forget to Create note because creating anything is always important
+                </p>
+               
+            </div>
+           
+        </div>       
+    </div>
+
+    
+</div>
   )
 }
 
